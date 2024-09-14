e30b3a9f7c6c24d7237e89731865fc9a8333396d
Omar Ledesma <joseomar.ledagui@gmail.com>
2024-09-14 12:12:29 +0100

    add mongo model and search configuration

--- SearchService/Program.cs
+++ SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,5 +15,16 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+var name = "MongoDbConnection";
+var connection = builder.Configuration.GetConnectionString(name);
+await DB.InitAsync("SearchDb", 
+    MongoClientSettings.FromConnectionString(connection));
+
+await DB.Index<Item>()
+    .Key(x => x.Make, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Color, KeyType.Text)
+    .CreateAsync();
 
 app.Run();
