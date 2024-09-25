38fd64c5ea4317e7b7d35984c48b969c1dbf4c9b
Omar Ledesma <joseomar.ledagui@gmail.com>
2024-09-25 11:35:48 +0100

    add register model get post

--- IdentityService/Pages/Account/Register/Index.cshtml.cs
+++ IdentityService/Pages/Account/Register/Index.cshtml.cs
@@ -1,6 +1,10 @@
+using IdentityModel;
+using IdentityService.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
 
 namespace IdentityService.Pages.Account.Register
 {
@@ -8,8 +12,55 @@ namespace IdentityService.Pages.Account.Register
     [AllowAnonymous]
     public class Index : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<ApplicationUser> _userManager;
+        [BindProperty]
+        public RegisterViewModel Input { get; set; }
+        [BindProperty]
+        public bool RegisterSuccess { get; set; }
+
+
+        public Index(UserManager<ApplicationUser> userManager)
         {
+            _userManager = userManager;
+        }
+        public IActionResult OnGet(string returnUrl)
+        {
+            Input = new RegisterViewModel { ReturnUrl = returnUrl };
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (Input.Button != "register") return Redirect("~/");
+
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = Input.Username,
+                    Email = Input.Email,
+                    EmailConfirmed = true
+                };
+
+                var result = await _userManager.CreateAsync(user, Input.Password);
+
+                if (result.Succeeded)
+                {
+                    await _userManager.AddClaimsAsync(user,
+                    [
+                        new Claim(ClaimTypes.Name, Input.Username)
+                    ]);
+                    /*await _userManager.AddClaimsAsync(user, new Claim[]
+                    {
+                        new Claim(ClaimTypes.Name, Input.Username)
+                    );*/
+                }
+
+                RegisterSuccess = true;
+            }
+
+            return Page();
         }
     }
 }
