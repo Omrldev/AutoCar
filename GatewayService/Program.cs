821ca990a9ca6d9809dfc78c1bf8ac58dca24d82
Omar Ledesma <joseomar.ledagui@gmail.com>
2024-10-06 17:12:20 +0100

    testing gateway service

--- GatewayService/Program.cs
+++ GatewayService/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,9 +8,21 @@ var name = "ReverseProxy";
 var proxy = builder.Configuration.GetSection(name);
 builder.Services.AddReverseProxy().LoadFromConfig(proxy);
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opt =>
+    {
+        opt.Authority = builder.Configuration["IdentityServiceUrl"];
+        opt.RequireHttpsMetadata = false;
+        opt.TokenValidationParameters.ValidateAudience = false;
+        opt.TokenValidationParameters.NameClaimType = "username";
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapReverseProxy();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
