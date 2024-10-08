19ff9ea231363819cb7f539ca2258cac16065c16
Omar Ledesma <joseomar.ledagui@gmail.com>
2024-10-08 13:14:23 +0100

    add two more consumers to search service

--- Contracts/BidPlaced.cs
+++ Contracts/BidPlaced.cs
@@ -12,7 +12,7 @@ namespace Contracts
         public string AuctionId { get; set; }
         public string Bidder { get; set; }
         public DateTime BidTime { get; set; }
-        public int? Amount { get; set; }
+        public int Amount { get; set; }
         public string BidStatus { get; set; }
     }
 }
